b313e77569e6072cf06b56121aa746567dfbe467
Brian Barnes <brianb12321@users.noreply.github.com>
2024-02-03 14:15:12 -0600

    Fix build error

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 builder.Services.AddStackExchangeRedisCache(options => {
     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance"
+    options.InstanceName = "SampleInstance";
 });
 
 // Add services to the container.
